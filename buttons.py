9ab12e9a037dee9183de15c4a733cf6565e7af99
railg <roblox.master@mail.ru>
2023-11-21 19:35:23 +0300

    keyboard v1.1

--- buttons.py
+++ buttons.py
@@ -8,29 +8,27 @@ class Button:
     def start(self) -> VkKeyboard:
         start_key = VkKeyboard(inline=False, one_time=True)
         start_key.add_button('Начать поиск', color=VkKeyboardColor.SECONDARY)
-        return start_key
+        return start_key.get_keyboard()
     
     def next(self) -> VkKeyboard:
         next_key = VkKeyboard(inline=False, one_time=True)
         next_key.add_button('Далее', color=VkKeyboardColor.SECONDARY)
-        return next_key
-    
-    def show_liked_photos(self) -> VkKeyboard:
-        s_l_p = VkKeyboard(inline=False, one_time=True)
-        s_l_p.add_button('Показать понравившиеся фотографии', color=VkKeyboardColor.POSITIVE)
-        return s_l_p
-    
-    def black_list(self) -> VkKeyboard:
-        black_list_key = VkKeyboard(inline=False, one_time=True)
-        black_list_key.add_button('Добавить в чёрный список', color=VkKeyboardColor.NEGATIVE)
-        return black_list_key
-    
-    def show_black_list(self) -> VkKeyboard:
-        show_list = VkKeyboard(inline=False, one_time=True)
-        show_list.add_button('Показать чёрный список', color=VkKeyboardColor.SECONDARY)
-        return show_list
+        next_key.add_button('Начать все сначало', color=VkKeyboardColor.PRIMARY)
+        next_key.add_line()
+        next_key.add_button('Показать понравившиеся фотографии', color=VkKeyboardColor.POSITIVE)
+        next_key.add_button('Показать чёрный список', color=VkKeyboardColor.SECONDARY)
+        next_key.add_line()
+        next_key.add_button('Добавить в чёрный список', color=VkKeyboardColor.NEGATIVE)
+        return next_key.get_keyboard()
     
     def delete_black_list(self) -> VkKeyboard:
         delete_black_list = VkKeyboard(inline=False, one_time=True)
         delete_black_list.add_button('Удалить из чёрного списка', color=VkKeyboardColor.NEGATIVE)
-        return delete_black_list
+        delete_black_list.add_button('Назад', color=VkKeyboardColor.NEGATIVE)
+        return delete_black_list.get_keyboard()
+    
+    def yes_or_no(self) -> VkKeyboard:
+        yes_or_no = VkKeyboard(inline=False, one_time=True)
+        yes_or_no.add_button('Да, хочу начать сначала', color=VkKeyboardColor.NEGATIVE)
+        yes_or_no.add_button('Нет, я передумал', color=VkKeyboardColor.SECONDARY)
+        return yes_or_no.get_keyboard()
