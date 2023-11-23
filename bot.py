c10d1179b322a098b237f17db3edca9add756185
railg <roblox.master@mail.ru>
2023-11-23 22:37:31 +0300

    v0.3

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import vk_api
+from datetime import datetime
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
 from config import USER_TOKEN, GROUP_TOKEN, GROUP_ID
@@ -16,19 +17,45 @@ class VKinder():
 
     # user_id
     def user_name(self) -> int:
-        user_name = self.vk_user.method('users.get')[0]['name']
+        user_name = self.vk_user.method('users.get')[0]['first_name']
         return user_name
     
     # user_bdate
-    def user_bdate(self) -> str:
-        user_bdate = self.vk_user.method('users.get', {'fields': 'bdate'})
-        return user_bdate
+    def user_bdate(self) -> int:
+        user_info = self.vk_user.method('users.get', {'fields': 'bdate'})
+        user_bdate = user_info[0].get('bdate')
+        user_id = user_info[0].get('id')
+        if user_bdate == None:
+            self.bot_write(user_id, 'Пожалуйста напишите, сколько вам лет?')
+            for event in self.bot_longpoll.listen():
+                if event.type == VkBotEventType.MESSAGE_NEW:
+                    peer_id = event.object.message['peer_id']
+                    if peer_id == user_id:
+                        age = event.object.message['text']
+                        if int(age) < 100:
+                            return age
+                        else:
+                            self.bot_write(user_id, 'А вы точно человек? Введите реальный возраст.')
+        else:
+            age = self.calculate_age(user_bdate)
+            return age
     
     # user_city
     def user_city(self) -> str:
         user_city = self.vk_user.method('users.get', {'fields': 'city'})[0]
+        user_id = user_city.get('id')
         if user_city.get('city') == None:
-            return None
+            self.bot_write(user_id, 'Пожалуйста введите ваш город: ')
+            for event in self.bot_longpoll.listen():
+                if event.type == VkBotEventType.MESSAGE_NEW:
+                    peer_id = event.object.message['peer_id']
+                    if peer_id == user_id:
+                        city = event.object.message['text']
+                        citys_db = self.vk_user.method('database.getCities', {'q': city})
+                        user_city_title = citys_db['items'][0].get('title')
+                        if user_city_title.lowe() == city.lower():
+                            user_city_id = citys_db['items'][0].get('id')
+                            return (user_city_id, user_city_title)
         user_city_id = user_city.get('id')
         user_city_title = user_city.get('title')
         return (user_city_id, user_city_title)
@@ -41,9 +68,11 @@ class VKinder():
         return 1
     
     # find_users
-    def search_partner(self, city_id: tuple, user_sex: int, user_bdate: str) -> None:
+    def search_partner(self, city_id: tuple | None, user_sex: int, user_bdate: str) -> None:
         user_id_city = self.user_city()
         user_id = self.vk_user.method('users.get')[0]['id']
+        if city_id == None:
+            pass
         search_result = self.vk_user.method('users.search', {'city': user_id_city[0], 'sex': self.user_sex(), 'age_from': self.user_bdate()-3, 'age_to': self.user_bdate()+3})
         if search_result.get('count') == 0:
             return self.bot_write(user_id, 'Нам очень жаль, что вы не смогли найти вашего партнёра(-шу) в городе {}'.format(user_id_city[1]))
@@ -68,15 +97,27 @@ class VKinder():
         for i in photo_id:
             self.vk_group.method('messages.send', {'user_id': user_id, 'random_id': 0, 'attachment': f'photo348914127_{i}', 'keyboard': keyboard.get_keyboard()})
             for event in self.bot_longpoll.listen():
+                peer_id = event.object.message['peer_id']
                 if event.type == VkBotEventType.MESSAGE_NEW:
-                    if event.object.message['text'] == 'Like':
-                        self.vk_group.method('messages.send', {'user_id': user_id, 'random_id': 0, 'message': "Фотография добавлена в понравившиеся."})
+                    if event.object.message['text'] == 'Like' and user_id == peer_id:
+                        self.bot_write(user_id, "Фотография добавлена в понравившиеся.")
                         #DB Update photo_likes to 1 ОДНУ ФУНКЦИЮ В КОТОРЫЙ ПАРАМЕТРОМ ДОАБВИТЬ 1 ИЛИ 0
-                    elif event.object.message['text'] == 'Dislike':
-                        self.vk_group.method('messages.send', {'user_id': user_id, 'random_id': 0, 'message': "Мы постараемся вам больше не показывать эту фотографию."})
+                    elif event.object.message['text'] == 'Dislike' and user_id == peer_id:
+                        self.bot_write(user_id, "Мы постараемся вам больше не показывать эту фотографию.")
                         #DB Update photo_likes to 0 ОДНУ ФУНКЦИЮ В КОТОРЫЙ ПАРАМЕТРОМ ДОАБВИТЬ 1 ИЛИ 0
                     break
     
+    # calculate age
+    def calculate_age(self, date: str) -> int:
+        bdate = date.split('.')
+        date_now = datetime.now()
+        age = date_now.year -  int(bdate[2])
+        if int(bdate[1]) > date_now.month:
+            return age - 1
+        elif int(bdate[1]) == date_now.month and int(bdate[0]) > date_now.day:
+            return age - 1
+        else:
+            return age
     # DB_users
     # DB_
     # DB_
