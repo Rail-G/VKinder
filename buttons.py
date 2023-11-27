b9140e900022a98c2ab320ea0ac837475c903518
railg <roblox.master@mail.ru>
2023-11-27 20:42:55 +0300

    bug fix

--- buttons.py
+++ buttons.py
@@ -31,7 +31,7 @@ class Button:
     def delete_black_list(self) -> VkKeyboard:
         delete_black_list = VkKeyboard(inline=False, one_time=True)
         delete_black_list.add_button('Удалить из чёрного списка', color=VkKeyboardColor.NEGATIVE)
-        delete_black_list.add_button('Назад в меню.', color=VkKeyboardColor.PRIMARY)
+        delete_black_list.add_button('В главное меню', color=VkKeyboardColor.PRIMARY)
         return delete_black_list.get_keyboard()
     
     def yes_or_no(self) -> VkKeyboard:
@@ -43,13 +43,13 @@ class Button:
     def delete_photo(self):
         delete_photo = VkKeyboard(inline=False, one_time=True)
         delete_photo.add_button('Удалить фото из понравивщихся', color=VkKeyboardColor.PRIMARY)
-        delete_photo.add_button('Назад в меню.', color=VkKeyboardColor.PRIMARY)
+        delete_photo.add_button('В главное меню', color=VkKeyboardColor.PRIMARY)
         return delete_photo.get_keyboard()
     
     def delete_favorite(self):
         delete_favorite = VkKeyboard(inline=False, one_time=True)
         delete_favorite.add_button('Удалить из избранных', color=VkKeyboardColor.NEGATIVE)
-        delete_favorite.add_button('Назад в меню.', color=VkKeyboardColor.PRIMARY)
+        delete_favorite.add_button('В главное меню', color=VkKeyboardColor.PRIMARY)
         return delete_favorite.get_keyboard()
     
     def back_to_menu(self):
