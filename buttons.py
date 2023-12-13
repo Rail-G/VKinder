93ebdbf251e2b2d17105f07efd00d4124742171f
railg <roblox.master@mail.ru>
2023-12-13 16:13:11 +0300

    Linter fix

--- buttons.py
+++ buttons.py
@@ -1,11 +1,14 @@
+"""Модули для работы с buttons.py"""
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 class Button:
+    """Documentation in documentation.md"""
 
     def __init__(self) -> None:
         pass
 
     def start(self) -> VkKeyboard.get_keyboard:
+        """Documentation in documentation.md"""
         start_key = VkKeyboard(inline=False, one_time=True)
         start_key.add_button('Начать поиск', color=VkKeyboardColor.POSITIVE)
         start_key.add_button('Вернуться к поиску', color=VkKeyboardColor.POSITIVE)
@@ -16,8 +19,9 @@ class Button:
         start_key.add_line()
         start_key.add_button('Показать избранных пользователей', color=VkKeyboardColor.PRIMARY)
         return start_key.get_keyboard()
-    
+
     def next(self) -> VkKeyboard.get_keyboard:
+        """Documentation in documentation.md"""
         next_key = VkKeyboard(inline=False, one_time=True)
         next_key.add_button('Далее', color=VkKeyboardColor.PRIMARY)
         next_key.add_button('Начать все сначало', color=VkKeyboardColor.PRIMARY)
@@ -27,38 +31,44 @@ class Button:
         next_key.add_button('Добавить в избранные', color=VkKeyboardColor.PRIMARY)
         next_key.add_button('В главное меню', color=VkKeyboardColor.PRIMARY)
         return next_key.get_keyboard()
-    
+
     def delete_black_list(self) -> VkKeyboard.get_keyboard:
+        """Documentation in documentation.md"""
         delete_black_list = VkKeyboard(inline=False, one_time=True)
         delete_black_list.add_button('Удалить из чёрного списка', color=VkKeyboardColor.NEGATIVE)
         delete_black_list.add_button('В главное меню', color=VkKeyboardColor.PRIMARY)
         return delete_black_list.get_keyboard()
-    
+
     def yes_or_no(self) -> VkKeyboard.get_keyboard:
+        """Documentation in documentation.md"""
         yes_or_no = VkKeyboard(inline=False, one_time=True)
         yes_or_no.add_button('Удалить все данные!', color=VkKeyboardColor.NEGATIVE)
         yes_or_no.add_button('Нет, я передумал', color=VkKeyboardColor.PRIMARY)
         return yes_or_no.get_keyboard()
-    
+
     def delete_photo(self) -> VkKeyboard.get_keyboard:
+        """Documentation in documentation.md"""
         delete_photo = VkKeyboard(inline=False, one_time=True)
         delete_photo.add_button('Удалить фото из понравивщихся', color=VkKeyboardColor.PRIMARY)
         delete_photo.add_button('В главное меню', color=VkKeyboardColor.PRIMARY)
         return delete_photo.get_keyboard()
-    
+
     def delete_favorite(self) -> VkKeyboard.get_keyboard:
+        """Documentation in documentation.md"""
         delete_favorite = VkKeyboard(inline=False, one_time=True)
         delete_favorite.add_button('Удалить из избранных', color=VkKeyboardColor.NEGATIVE)
         delete_favorite.add_button('В главное меню', color=VkKeyboardColor.PRIMARY)
         return delete_favorite.get_keyboard()
-    
+
     def back_to_menu(self) -> VkKeyboard.get_keyboard:
+        """Documentation in documentation.md"""
         back = VkKeyboard(inline=False, one_time=True)
         back.add_button('В главное меню', color=VkKeyboardColor.PRIMARY)
         return back.get_keyboard()
-        
+
     def like_dislike(self) -> VkKeyboard.get_keyboard:
-        keyboard = VkKeyboard(inline=True) 
+        """Documentation in documentation.md"""
+        keyboard = VkKeyboard(inline=True)
         keyboard.add_button('Like', color=VkKeyboardColor.SECONDARY)
         keyboard.add_button('Dislike', color=VkKeyboardColor.SECONDARY)
         return keyboard.get_keyboard()
